74ce59c1ae7381438fd23e94da095605ac5cbeb3
Project-start-2025 <211314620+Project-start-2025@users.noreply.github.com>
2025-05-14 02:12:09 +0700

    Update Topbar.tsx

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -1,10 +1,18 @@
 // components/Topbar.tsx
 const Topbar = () => {
-    return (
-      <div style={{ backgroundColor: '#FFC107', color: '#333', fontWeight: '600', textAlign: 'center', padding: '6px' }}>
-        MIỄN PHÍ VẬN CHUYỂN TRÊN MỌI ĐƠN HÀNG
-      </div>
-    );
-  };
-  
-  export default Topbar;
+  return (
+    <div
+      style={{
+        backgroundColor: "#FFC107",
+        color: "#333",
+        fontWeight: "600",
+        textAlign: "center",
+        padding: "6px",
+      }}
+    >
+      MIỄN PHÍ VẬN CHUYỂN TRÊN MỌI ĐƠN HÀNG
+    </div>
+  );
+};
+
+export default Topbar;
