717417eb6d4f3ad3ffbf2e93059bb6237f790522
Project-start-2025 <211314620+Project-start-2025@users.noreply.github.com>
2025-05-14 19:45:05 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,10 @@ import Topbar from "./components/Topbar";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>
+      <body style={{ margin: 0, padding: 0 }}>
         <Topbar />
         <Header />
         {children}
