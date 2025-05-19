96fa558a20efd874607abdb92eff5f1cc0d2f857
Project-start-2025 <211314620+Project-start-2025@users.noreply.github.com>
2025-05-19 14:54:52 +0700

    Create VendorNav.tsx

---
+++ src/app/components/vendor/VendorNav.tsx
@@ -0,0 +1,14 @@
+// components/VendorNav.tsx
+import React from "react";
+import VendorNavItem from "./VendorNavItem";
+
+const VendorNav = () => {
+  return (
+    <nav className="bg-white border-b shadow-sm px-4 py-3 flex space-x-4">
+      <VendorNavItem href="/vendor/add-products" label="➕ Thêm sản phẩm" />
+      <VendorNavItem href="/vendor/statistics" label="📊 Thống kê doanh số" />
+    </nav>
+  );
+};
+
+export default VendorNav;
