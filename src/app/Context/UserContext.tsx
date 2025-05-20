37624a989e95e2acc6519fe5552885bc4a396abb
Project-start-2025 <211314620+Project-start-2025@users.noreply.github.com>
2025-05-20 21:26:38 +0700

    Create UserContext.tsx

---
+++ src/app/Context/UserContext.tsx
@@ -0,0 +1,38 @@
+
+"use client";
+import React, { createContext, useState, useContext, ReactNode } from "react";
+
+interface IUser {
+  username: string;
+  fullname: string;
+  role: string;
+}
+
+interface IUserContext {
+  user: IUser | null;
+  setUser: (user: IUser | null) => void;
+  logout: () => void;
+}
+
+const UserContext = createContext<IUserContext | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<IUser | null>(null);
+
+  const logout = () => {
+    setUser(null);
+    
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) throw new Error("useUser must be used within UserProvider");
+  return context;
+};
