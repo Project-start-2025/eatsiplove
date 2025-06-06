5c0bc4ab423e7248d3728199c08fa702eff5295f
Project-start-2025 <211314620+Project-start-2025@users.noreply.github.com>
2025-06-07 04:39:56 +0700

    Create Products.ts

---
+++ src/model/Products.ts
@@ -0,0 +1,28 @@
+import {Entity,PrimaryGeneratedColumn,Column, CreateDateColumn,ManyToOne, OneToMany, JoinColumn } from "typeorm";
+import { Account } from "./Account";
+  @Entity()
+  export class Products {
+    @PrimaryGeneratedColumn()
+    id!: number;
+    @Column()
+    name!: string;
+    @Column({ type: "text" })
+    description!: string;
+    @Column({ type: "float" })
+    price!: number;
+    @Column({ type: "nvarchar", length: "max", nullable: true })
+    images!: string; 
+    @Column({ default: 0 })
+    stock!: number;
+    @Column({ type: "float", default: 0 })
+    averageRating!: number;
+    @CreateDateColumn({ type: "datetime2" })
+    createdAt!: Date;
+    @ManyToOne(() => Account)
+    @JoinColumn({ name: "userId" })
+    user!: Account;
+    // @OneToMany(() => Review, review => review.product)
+    // reviews!: Review[];
+    // @OneToMany(() => OrderOnProduct, o => o.product)
+    // orderItems!: OrderOnProduct[];
+  }
