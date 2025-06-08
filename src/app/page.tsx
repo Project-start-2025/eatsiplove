76947c435d62c28431b458ecaa5742903556edb6
Project-start-2025 <211314620+Project-start-2025@users.noreply.github.com>
2025-06-08 19:59:38 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
-import Image from "next/image";
+import { Food } from "@/model/Food";
+import { useEffect, useState } from "react";
+import FoodList from "./components/Food/FoodList";
+
 
 export default function Home() {
-  return <div>ThienPhuoc</div>;
+  const [foods, setFoods] = useState<Food[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/api/Food")
+      .then((res) => res.json())
+      .then((data) => {
+        setFoods(data.foods);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <main>
+      <h1>Danh sách món ăn</h1>
+      <FoodList foods={foods} />
+    </main>
+  );
 }
