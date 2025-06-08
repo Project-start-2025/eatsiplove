8194604d31147b85359c19d820da17f9199a0829
Project-start-2025 <211314620+Project-start-2025@users.noreply.github.com>
2025-06-08 19:59:55 +0700

    Update typeorm.ts

--- src/lib/typeorm.ts
+++ src/lib/typeorm.ts
@@ -1,15 +1,15 @@
-  import { DataSource } from "typeorm";
-  import { Account } from "@/model/Account";
-import { Products } from "@/model/Products";
+import { DataSource } from "typeorm";
+import { Account } from "@/model/Account";
+import { Food } from "@/model/Food";
 
-  export const AppDataSource = new DataSource({
-    type: "mssql",
-    host: process.env.DB_SERVER,
-    port: 1433,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    entities: [Products, Account],
-    synchronize: true,
-    options: { encrypt: true }
-  });
+export const AppDataSource = new DataSource({
+  type: "mssql",
+  host: process.env.DB_SERVER,
+  port: 1433,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Food, Account],
+  synchronize: true,
+  options: { encrypt: true },
+});
