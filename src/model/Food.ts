12dc6814bd912a2dff88286a95f3eef885ce1356
Project-start-2025 <211314620+Project-start-2025@users.noreply.github.com>
2025-06-10 21:48:13 +0700

    Update Food.ts

--- src/model/Food.ts
+++ src/model/Food.ts
@@ -1,37 +1,40 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  ManyToOne,
-  OneToMany,
-  JoinColumn,
-} from "typeorm";
-import { Account } from "./Account";
-@Entity()
-export class Food {
-  @PrimaryGeneratedColumn()
-  id!: number;
-  @Column()
-  name!: string;
-  @Column({ type: "text" })
-  description!: string;
-  @Column({ type: "float" })
-  price!: number;
-  @Column({ type: "nvarchar", length: "max", nullable: true })
-  images!: string;
-  @Column({ default: 0 })
-  stock!: number;
-  @Column({ type: "float", default: 0 })
-  averageRating!: number;
-  @CreateDateColumn({ type: "datetime2" })
-  createdAt!: Date;
-  @ManyToOne(() => Account)
-  @JoinColumn({ name: "userId" })
-  user!: Account;
-  product: any;
-  // @OneToMany(() => Review, review => review.product)
-  // reviews!: Review[];
-  // @OneToMany(() => OrderOnProduct, o => o.product)
-  // orderItems!: OrderOnProduct[];
-}
+    import {
+      Entity,
+      PrimaryGeneratedColumn,
+      Column,
+      CreateDateColumn,
+      ManyToOne,
+      OneToMany,
+      JoinColumn,
+    } from "typeorm";
+import { Restaurant } from "./Restaurant";
+    @Entity()
+    export class Food {
+      @PrimaryGeneratedColumn()
+      id!: number;
+      @Column()
+      name!: string;
+      @Column({ type: "text" })
+      description!: string;
+      @Column({ type: "float" })
+      price!: number;
+      @Column({ type: "nvarchar", length: "max", nullable: true })
+      images!: string;
+      @Column({ default: 0 })
+      stock!: number;
+      @Column({ type: "float", default: 0 })
+      averageRating!: number;
+      @CreateDateColumn({ type: "datetime2" })
+      createdAt!: Date;
+      @ManyToOne(() => Restaurant)
+      @JoinColumn({ name: "restaurantId" })  // 👈 Khoá ngoại đúng là ở đây
+      restaurant!: Restaurant;
+      // @ManyToOne(() => Account)
+      // @JoinColumn({ name: "userId" })
+      // user!: Account;
+      // product: any;
+      // @OneToMany(() => Review, review => review.product)
+      // reviews!: Review[];
+      // @OneToMany(() => OrderOnProduct, o => o.product)
+      // orderItems!: OrderOnProduct[];
+    }
